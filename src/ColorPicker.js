f28e88ada42dc10f531533459b08e66e2f4d1354
Chris <chrispackett1998@gmail.com>
2018-08-15 15:49:32 -0400

    made event handlers, implemented dynamic hsl background-color

--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -10,15 +10,36 @@ class ColorPicker extends Component {
         }
     }
     
+    updateHue = (event) => {
+        let updatedHue = event.target.value
+        this.setState({
+            hue: updatedHue
+        }) 
+    }
+
+    updateSaturation = (event) => {
+        let updatedSaturation = event.target.value
+        this.setState({
+            saturation: updatedSaturation
+        }) 
+    }
+
+    updateLightness = (event) => {
+        let updatedLightness = event.target.value
+        this.setState({
+            lightness: updatedLightness
+        }) 
+    }
+
     render() {
         return (
             <section className='color-palette'>
-                <section className='box-style'>
+                <section className='box-style' style={{ backgroundColor: `hsl(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%)` }}>
                 </section>
                 <section className='hsl-sliders'>
-                    <input type='range' min='0' max='360' value={this.state.hue}/>
-                    <input type='range' min='0' max='100' value={this.state.saturation}/>
-                    <input type='range' min='0' max='100' value={this.state.lightness}/>
+                    <input type='range' min='0' max='360' value={this.state.hue} onChange={this.updateHue}/>
+                    <input type='range' min='0' max='100' value={this.state.saturation} onChange={this.updateSaturation}/>
+                    <input type='range' min='0' max='100' value={this.state.lightness} onChange={this.updateLightness}/>
                 </section>
             </section>
         );
